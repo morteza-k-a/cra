27121a5ff02f4e0833017770cb6a777afc315bd0
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2020-07-23 00:27:15 +0430

    Updates

---
+++ precache-manifest.7d1e20f47d5544d129b901d8405216ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42aebcd20d857626d56b6f5a2f9c1671",
+    "url": "/cra/index.html"
+  },
+  {
+    "revision": "90b41849627ea141067b",
+    "url": "/cra/static/css/main.9eed47e9.chunk.css"
+  },
+  {
+    "revision": "361fe9b6fc836f4eb356",
+    "url": "/cra/static/js/2.bfca6209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra/static/js/2.bfca6209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b41849627ea141067b",
+    "url": "/cra/static/js/main.02bc3c7b.chunk.js"
+  },
+  {
+    "revision": "c23289210c523185aec0",
+    "url": "/cra/static/js/runtime-main.b1aa1615.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra/static/media/logo.5d5d9eef.svg"
+  }
+]);
